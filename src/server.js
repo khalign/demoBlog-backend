01204a96511ebe2c6a3da3bffe4e36387fb8a194
khalig-n <khalig.novruzli@gmail.com>
2019-11-11 15:02:51 +0300

    connected with mondoDB

--- src/server.js
+++ src/server.js
@@ -1,38 +1,60 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {MongoClient} from 'mongodb';
 
 const app = express();
 app.use(bodyParser.json());
 
-const mockData = {
-    'learn-react': {
-        upvotes: 0,
-        comments: [],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: [],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: [],
-    },
-}
+const withDB = async (actions, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('demo-blog');
 
-app.post('/api/articles/:name/like', (req, res) => {
-    const article = req.params.name;
+        await actions(db);
 
-    mockData[article].upvotes += 1;
-    res.status(200).send(`${article} now has ${mockData[article].upvotes} upvotes!`);
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });        
+    }
+} 
+
+
+app.get('/api/articles/:name', (req, res) => {
+    const {name} = req.params;
+
+    withDB(async db => {
+        const data = await db.collection('articles').findOne({ name })
+        res.status(200).json(data);
+    }, res);
+})
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const {name} = req.params;
+    
+    withDB(async db => {
+        const data = await db.collection('articles').findOne({ name });
+        await db.collection('articles').updateOne({ name }, {
+            '$set': { upvotes: data.upvotes + 1 }
+        });
+        const updated = await db.collection('articles').findOne({ name });
+    
+        res.status(200).json(updated);
+    }, res);
 });
 
-app.post('/api/articles/:name/comment', (req, res) => {
+app.post('/api/articles/:name/add-comment', (req, res) => {
     const { username, text } = req.body;
-    const article = req.params.name;
+    const {name} = req.params;
 
-    mockData[article].comments.push({ username, text });
+    withDB(async db => {
+        const data = await db.collection('articles').findOne({ name });
+        await db.collection('articles').updateOne({ name }, {
+            '$set': { comments: data.comments.concat({ username, text }) }
+        });
+        const updated = await db.collection('articles').findOne({ name });
 
-    res.status(200).send(mockData[article]);
+        res.status(200).json(updated);
+    }, res);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
